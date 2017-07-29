8b7186becf1c0a3b599c2c41d26742a6ec9cb681
Tomislav Pesut <tomca32@gmail.com>
2017-07-29 16:53:57 -0400

    Working Recycler view

--- app/src/main/java/io/tomislav/movies/popularmovies/MovieGridActivity.java
+++ app/src/main/java/io/tomislav/movies/popularmovies/MovieGridActivity.java
@@ -1,22 +1,99 @@
 package io.tomislav.movies.popularmovies;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MovieGridActivity extends AppCompatActivity {
 
-    private ImageView imageView;
+    OkHttpClient client = new OkHttpClient();
+
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_grid);
 
-        imageView = (ImageView) findViewById(R.id.popularMovie);
+        recyclerView = (RecyclerView) findViewById(R.id.rv_movies_grid);
+        GridLayoutManager manager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(manager);
 
-        Picasso.with(this).load("http://i.imgur.com/DvpvklR.png").into(imageView);
+        GetMoviesTask task = new GetMoviesTask();
+
+
+        task.execute(getPopularMoviesUrl());
+    }
+
+
+    private URL getPopularMoviesUrl() {
+        URL url;
+        try {
+            url = new URL(getString(R.string.popular_movies_url) + "=" + getString(R.string.movie_db_api_key));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return url;
+    }
+
+    void updateMovieGrid(JSONObject movies) {
+        Logger.getAnonymousLogger().log(Level.INFO, movies.toString());
+        try {
+            MovieGridAdapter adapter = new MovieGridAdapter((JSONArray) movies.get("results"), this);
+            recyclerView.setAdapter(adapter);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    class GetMoviesTask extends AsyncTask<URL, Void, JSONObject> {
+
+        @Override
+        protected JSONObject doInBackground(URL... params) {
+            URL url = params[0];
+            JSONObject result = null;
+
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
+            try {
+                Response response = client.newCall(request).execute();
+                result = new JSONObject(response.body().string());
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            } catch (JSONException e) {
+                e.printStackTrace();
+                return null;
+            }
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject result) {
+            updateMovieGrid(result);
+        }
     }
 }
---
+++ app/src/main/java/io/tomislav/movies/popularmovies/MovieGridAdapter.java
@@ -0,0 +1,74 @@
+package io.tomislav.movies.popularmovies;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import java.net.URL;
+
+class MovieGridAdapter extends RecyclerView.Adapter<MovieGridAdapter.PosterViewHolder> {
+
+    private JSONArray movies;
+    private Context context;
+
+    MovieGridAdapter(JSONArray movies, Context context) {
+        super();
+        this.movies = movies;
+        this.context = context;
+    }
+
+    @Override
+    public PosterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+
+        View view = inflater.inflate(R.layout.movie_poster_grid_item, parent, false);
+
+        return new PosterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(PosterViewHolder holder, int position) {
+        holder.bind(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.length();
+    }
+
+    class PosterViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView posterImageView;
+
+        PosterViewHolder(View itemView) {
+            super(itemView);
+
+            posterImageView = (ImageView) itemView.findViewById(R.id.iv_movie_poster_grid_item);
+        }
+
+        void bind(int index) {
+            String url = getPosterUrlFor(index);
+            Picasso.with(context).load(url).into(posterImageView);
+        }
+
+        private String getPosterUrlFor(int index) {
+            String baseImageUrl = context.getString(R.string.base_image_url);
+            String posterPath = null;
+            try {
+                posterPath = movies.getJSONObject(index).getString("poster_path");
+            } catch (JSONException e) {
+                e.printStackTrace();
+                return null;
+            }
+
+            return baseImageUrl + "w185" + posterPath;
+        }
+    }
+}
